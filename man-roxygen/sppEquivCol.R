a856e63c3357da353525f5a3d5d0d22fae989635
Ian Eddy <ianmseddy@gmail.com>
2020-09-04 13:43:13 -0700

    package stuff

--- man-roxygen/sppEquivCol.R
+++ man-roxygen/sppEquivCol.R
@@ -1,3 +1,1 @@
 #' @param sppEquivCol the column name to use from \code{sppEquiv}.
-#'              Only necessary if \code{mixedType == 2}.
-#'              If not provided and \code{mixedType == 2}, will attempt to use "Boreal".
