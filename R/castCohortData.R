d2a7bf94beb41af0a5638b5a4fc12c9df64e93a8
Ian Eddy <ianmseddy@gmail.com>
2020-12-18 10:30:10 -0800

    Several changes to fix standAge

--- R/castCohortData.R
+++ R/castCohortData.R
@@ -1,23 +1,25 @@
 globalVariables(c(
-  ".", "age", "B", "pixelGroup", "standAge"
+  ".", "age", "B", "pixelGroup", "standAge", "nonforest", "youngAge"
 ))
 
 #' preparing covariates for fitting modules
 #'
-#' @param cohortData DESCRIPTION NEEDED
+#' @template cohortData
 #' @param terrainDT optional data table of terrain covariates with pixelIndex
 #' @param lcc data.table of dummified landcover
 #' @param pixelGroupMap pixelGroupMap to join terrain with pixelGroup
+#' @param ageMap a stand age map to assign ages to non-forest LCC
+#' @param missingLCC Pixels on forested LCC but absent from cohortData will be assigned this LCC.
+#' must be a character matching a nonForestedLCC group, e.g. 'nonForest_highFlam'
 #'
-#' @return a trimmed cohortData with wide-layout
+#' @return a trimmed cohortData with wide-layout and rows for every pixel in lcc
 #'
 #' @export
 #' @importFrom data.table copy dcast set setnafill
 #' @rdname castCohortData
-castCohortData <- function(cohortData, terrainDT = NULL, pixelGroupMap, lcc) {
-  cohortData <- copy(cohortData)
-  #this can be made more efficient if the terrain data.table is constructed outside the function and passed in...
-  #need stand age for predictions
+castCohortData <- function(cohortData, terrainDT = NULL, pixelGroupMap, lcc, ageMap, missingLCC) {
+
+  #need stand age for predictions but it won't be included in PCAer
   cohortData[, standAge := sum(B * age) / sum(B), .(pixelGroup)] # don't chain with magrittr
   cohortData <- dcast(cohortData, pixelGroup + standAge ~ speciesCode, value.var = c("B"), fill = 0)
 
@@ -26,11 +28,22 @@ castCohortData <- function(cohortData, terrainDT = NULL, pixelGroupMap, lcc) {
   rm(cohortDataLong)
   cohortData <- cohortData[lcc, on = c("pixelID")]
 
+  #reclassify forested lcc that is missing from cohortData as missingLCC group
+  lccNames <- colnames(lcc)[!colnames(lcc) %in% c("pixelID")]
+
+   #this should be faster
+  cohortData[, nonforest := rowSums(cohortData[, .SD, .SDcols = lccNames])]
+  cohortData[is.na(pixelGroup) & nonforest == 0, eval(missingLCC) := 1] #these are forest by LCC2005 and not LandR
+  set(cohortData, ,'nonforest', NULL)
+
+  cohortData[is.na(standAge), standAge := ageMap[cohortData[is.na(standAge)]$pixelID]]
   if (!is.null(terrainDT)) {
     cohortData <- cohortData[terrainDT, on = c("pixelID")]
     setnafill(cohortData, fill = 0, cols = colnames(cohortData)[!colnames(cohortData) %in%
                                                                   c('pixelGroup', colnames(terrainDT))])
   }
+  cohortData[, youngAge := ifelse(standAge <= 15, 1, 0)]
+  set(cohortData, NULL, 'standAge', NULL) #we only care if stand age is young
 
   return(cohortData)
 }
--- R/makeVegTerrainPCA.R
+++ R/makeVegTerrainPCA.R
@@ -18,21 +18,27 @@ globalVariables(c(
 makeVegTerrainPCA <- function(dataForPCA, PCAmodel = NULL) {
   if (is.null(PCAmodel)) {
     #year should be present in data
-    vegTerrainPCA <- prcomp(dataForPCA[, .SD, .SDcols = !c("pixelGroup", 'pixelID', 'year')],
+    vegTerrainPCA <- prcomp(dataForPCA[, .SD, .SDcols = !c("pixelGroup", "pixelID", "year", "youngAge")],
                             center = TRUE, scale. = TRUE, rank = 10)
   } else{
     #To be tested
     vegTerrainPCA <- prcomp(object = PCAmodel,
-                            newdata = dataForPCA[, .SD, .SDcols = !c("pixelGroup", 'pixelID')])
+                            newdata = dataForPCA[, .SD, .SDcols = !c("pixelGroup", "pixelID", "youngAge")])
   }
   # store as Integer
   vegComponents <- as.data.table(vegTerrainPCA$x * 1000)
   vegComponents <- vegComponents[, lapply(.SD, asInteger), .SDcols = colnames(vegComponents)]
-  vegComponents[, pixelID := dataForPCA$pixelID]
 
+  #put age and pixelID back in
+  set(vegComponents, NULL, "pixelID" ,dataForPCA$pixelID)
+  set(vegComponents, NULL, "youngAge", dataForPCA$youngAge)
+
+  #put year in if present (ie during fitting)
   if (!is.null(dataForPCA$year)) {
     vegComponents[, year := dataForPCA$year]
   }
 
-  return(list('vegComponents' = vegComponents, 'vegTerrainPCA' = vegTerrainPCA))
+  #put age back in
+
+  return(list("vegComponents" = vegComponents, "vegTerrainPCA" = vegTerrainPCA))
 }
