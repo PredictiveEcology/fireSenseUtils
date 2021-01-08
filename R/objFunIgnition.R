0415776bbd8bf3b765ba7d0e285a947657f8bae3
Alex Chubaty <achubaty@for-cast.ca>
2021-01-08 16:27:50 -0700

    cleaning up and fix errors
    @ianmseddy note that `objIgniton` and `objIginitonPW` renamed to `.objFunIgnition` and `.objFunIgnitionPW`, respectively. this matches equivalent for spread fit.

--- R/objFunIgnition.R
+++ R/objFunIgnition.R
@@ -1,14 +1,13 @@
-
 #' Handling piecewise terms in a formula
+#'
 #' @param variable DESCRIPTION NEEDED
 #' @param knot DESCRIPTION NEEDED
 #' @return DESCRIPTION NEEDED
-
+#'
 #' @export
 pw <- function(variable, knot) pmax(variable - knot, 0)
 
-
-#' order of magnitude
+#' Order of Magnitude
 #'
 #' @param x DESCRIPTION NEEDED
 #' @return DESCRIPTION NEEDED
@@ -16,18 +15,24 @@ pw <- function(variable, knot) pmax(variable - knot, 0)
 oom <- function(x) 10^(ceiling(log10(abs(x))))
 
 #' Extract the elements of the special terms, i.e. the variable and the knot value
+#'
 #' @param v DESCRIPTION NEEDED
 #' @param k DESCRIPTION NEEDED
+#'
 #' @return DESCRIPTION NEEDED
+#'
 #' @export
-extractSpecial <- function(v, k){
+extractSpecial <- function(v, k) {
   cl <- match.call()
-
-  if(missing(k)) stop("> Argument 'knotName' is missing (variable '", as.character(cl$v), "')")
-  else list(variable = if (is(cl$v, "AsIs")) cl$v else as.character(cl$v), knot = as.character(cl$k))
+  if (missing(k)) {
+    stop("> Argument 'knotName' is missing (variable '", as.character(cl$v), "')")
+  } else {
+    list(variable = if (is(cl$v, "AsIs")) cl$v else as.character(cl$v), knot = as.character(cl$k))
+  }
 }
 
-#' Objective function when no piece-wise model is used
+#' Objective function when no piecewise model is used
+#'
 #' @param params DESCRIPTION NEEDED
 #' @param linkinv DESCRIPTION NEEDED
 #' @param nll DESCRIPTION NEEDED
@@ -36,9 +41,12 @@ extractSpecial <- function(v, k){
 #' @param mm DESCRIPTION NEEDED
 #' @param mod_env DESCRIPTION NEEDED
 #' @param offset DESCRIPTION NEEDED
+#'
 #' @return DESCRIPTION NEEDED
+#'
 #' @export
-objIgnition <- function(params, linkinv, nll, sm, nx, mm, mod_env, offset) {
+#' @rdname objFunIgnition
+.objFunIgnition <- function(params, linkinv, nll, sm, nx, mm, mod_env, offset) {
   ## Parameters scaling
   params <- drop(params %*% sm)
 
@@ -47,12 +55,15 @@ objIgnition <- function(params, linkinv, nll, sm, nx, mm, mod_env, offset) {
   ## link implementation
   mu <- linkinv(mu)
 
-  if(any(mu <= 0) || anyNA(mu) || any(is.infinite(mu)) || length(mu) == 0) return(1e20)
-  else return(eval(nll, envir = as.list(environment()), enclos = mod_env))
+  if (any(mu <= 0) || anyNA(mu) || any(is.infinite(mu)) || length(mu) == 0) {
+    return(1e20)
+  } else {
+    return(eval(nll, envir = as.list(environment()), enclos = mod_env))
+  }
 }
 
-
 #' Function to pass to the optimizer - Piece-wise version
+#'
 #' @param params DESCRIPTION NEEDED
 #' @param formula DESCRIPTION NEEDED
 #' @param linkinv DESCRIPTION NEEDED
@@ -63,10 +74,12 @@ objIgnition <- function(params, linkinv, nll, sm, nx, mm, mod_env, offset) {
 #' @param mod_env DESCRIPTION NEEDED
 #' @param offset DESCRIPTION NEEDED
 #'
-#' @importFrom stats model.matrix
 #' @return DESCRIPTION NEEDED
+#'
 #' @export
-objIgnitionPW <- function(params, formula, linkinv, nll, sm, updateKnotExpr, nx, mod_env, offset) {
+#' @importFrom stats model.matrix
+#' @rdname objFunIgnitionPW
+.objFunIgnitionPW <- function(params, formula, linkinv, nll, sm, updateKnotExpr, nx, mod_env, offset) {
   ## Parameters scaling
   params <- drop(params %*% sm)
 
@@ -77,19 +90,27 @@ objIgnitionPW <- function(params, formula, linkinv, nll, sm, updateKnotExpr, nx,
   ## link implementation
   mu <- linkinv(mu)
 
-  if(any(mu <= 0) || anyNA(mu) || any(is.infinite(mu)) || length(mu) == 0) return(1e20)
-  else return(eval(nll, envir = as.list(environment()), enclos = mod_env))
+  if (any(mu <= 0) || anyNA(mu) || any(is.infinite(mu)) || length(mu) == 0) {
+    return(1e20)
+  } else {
+    return(eval(nll, envir = as.list(environment()), enclos = mod_env))
+  }
 }
 
-
-#' Nlminb wrapper
+#' \code{objNlminb}
+#'
+#' Wrapper around \code{stats::nlminb}
+#'
 #' @param start DESCRIPTION NEEDED
 #' @param objective DESCRIPTION NEEDED
 #' @param lower DESCRIPTION NEEDED
 #' @param upper DESCRIPTION NEEDED
 #' @param control DESCRIPTION NEEDED
+#'
 #' @return DESCRIPTION NEEDED
+#'
 #' @export
+#' @importFrom stats nlminb
 objNlminb <- function(start, objective, lower, upper, control) {
   nlminb.call <- quote(nlminb(start = start, objective = objective, lower = lower, upper = upper, control = control))
   nlminb.call[names(formals(objective)[-1L])] <- parse(text = formalArgs(objective)[-1L])
@@ -99,8 +120,7 @@ objNlminb <- function(start, objective, lower, upper, control) {
   i <- 1L
 
   ## When there is no convergence and restart is possible, run nlminb() again
-  while(as.integer(gsub("[\\(\\)]", "", regmatches(o$message, gregexpr("\\(.*?\\)", o$message))[[1L]])) %in% 7:14 & i < 3L)
-  {
+  while (as.integer(gsub("[\\(\\)]", "", regmatches(o$message, gregexpr("\\(.*?\\)", o$message))[[1L]])) %in% 7:14 & i < 3L) {
     i <- i + 1L
     o <- eval(nlminb.call)
   }
