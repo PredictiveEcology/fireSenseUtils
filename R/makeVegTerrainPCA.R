ecaf56a344d73305c52289ab27b67636eb640081
Eliot McIntire <eliotmcintire@gmail.com>
2021-01-29 16:42:49 -0800

    use a log(x + 1) transformation of the species data

--- R/makeVegTerrainPCA.R
+++ R/makeVegTerrainPCA.R
@@ -7,23 +7,31 @@ globalVariables(c(
 #' @param dataForPCA a data.table of fireSense covariates for each pixelID,
 #'  with additional cols pixelGroup and year if running for FS fit
 #' @param PCAmodel if predicting, the PCA model from the fitting stage
+#' @param dontAlter The columns that shouldn't be transformed with log (x + 1)
+#' @param dontWant The columns that shouldn't be part of the PCA
 #'
 #' @return a list of the PCA and a data.table with components converted to integer
 #'
 #' @export
-#' @importFrom data.table data.table
+#' @importFrom data.table data.table set setDT
 #' @importFrom LandR asInteger
 #' @importFrom stats prcomp
 #' @rdname makeVegTerrainPCA
-makeVegTerrainPCA <- function(dataForPCA, PCAmodel = NULL) {
+makeVegTerrainPCA <- function(dataForPCA, PCAmodel = NULL,
+                              dontAlter = c("TPI", "HLI", "nonForest_highFlam", "nonForest_lowFlam"),
+                              dontWant = c("pixelGroup", "pixelID", "year", "youngAge")) {
+
+  # Log the species data
+  doModify <- setdiff(colnames(dataForPCA), c(dontWant, dontAlter))
+  dataForPCA[, (doModify) := lapply(.SD, function(x) log(x + 1)), .SDcols = doModify]
+  dd <- dataForPCA[, !(..dontWant)]
+
   if (is.null(PCAmodel)) {
     #year should be present in data
-    vegTerrainPCA <- prcomp(dataForPCA[, .SD, .SDcols = !c("pixelGroup", "pixelID", "year", "youngAge")],
-                            center = TRUE, scale. = TRUE, rank = 10)
+    vegTerrainPCA <- prcomp(dd, center = TRUE, scale. = TRUE, rank = 10)
   } else{
     #To be tested
-    vegTerrainPCA <- prcomp(object = PCAmodel,
-                            newdata = dataForPCA[, .SD, .SDcols = !c("pixelGroup", "pixelID", "youngAge")])
+    vegTerrainPCA <- prcomp(object = PCAmodel, newdata = dd)
   }
   # store as Integer
   vegComponents <- as.data.table(vegTerrainPCA$x)
