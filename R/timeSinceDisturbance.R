adb02a096f0e094c4788f6a573c1278e008aad62
Eliot McIntire <eliotmcintire@gmail.com>
2021-02-05 00:52:11 +0000

    add cutoffForYoungAge; minor speedups

--- R/timeSinceDisturbance.R
+++ R/timeSinceDisturbance.R
@@ -8,6 +8,7 @@ globalVariables(c(
 #' @param firePolys list of \code{spatialPolygon} objects comprising annual fires
 #' @param year the year represented by \code{standAge}
 #' @param lcc \code{data.table} with landcover values - \code{landcoverDT}
+#' @inheritParams castCohortData
 #'
 #' @return a raster layer with values representing time since disturbance
 #'
@@ -17,10 +18,10 @@ globalVariables(c(
 #' @importFrom magrittr %>%
 #' @importFrom raster getValues raster setValues
 #' @importFrom sf st_collection_extract
-makeTSD <- function(year, firePolys, standAgeMap, lcc) {
+makeTSD <- function(year, firePolys, standAgeMap, lcc, cutoffForYoungAge = 15) {
   ## TODO: Make this work with lcc values that aren't hardcoded ie line 33
   ## get particular fire polys in format that can be fasterized
-  polysNeeded <- names(firePolys) %in% paste0("year", c(year - 16):year - 1) %>%
+  polysNeeded <- names(firePolys) %in% paste0("year", c(year - cutoffForYoungAge - 1):year - 1) %>%
     firePolys[.] %>%
     .[lengths(.) > 0] %>% #gets rid of missing years that break function
     lapply(., FUN = sf::st_as_sf) %>%
@@ -31,12 +32,16 @@ makeTSD <- function(year, firePolys, standAgeMap, lcc) {
 
   #create background raster with TSD
   initialTSD <-  sf::st_collection_extract(polysNeeded, "POLYGON") %>%
-    fasterize(sf = ., raster = standAgeMap, background = year - 16, field = "YEAR", fun = "max") %>%
+    fasterize(sf = ., raster = standAgeMap, background = year - cutoffForYoungAge - 1,
+              field = "YEAR", fun = "max") %>%
     setValues(., values = year - getValues(.))
 
   pixToUpdate <- lcc[nonForest_highFlam > 0 | nonForest_lowFlam > 0,]$pixelID
 
-  standAgeMap[pixToUpdate] <- initialTSD[pixToUpdate]
+  falseYoungs <- standAgeMap[][pixToUpdate] <= cutoffForYoungAge
+  trueYoungs <- initialTSD[pixToUpdate] <= cutoffForYoungAge
+  standAgeMap[pixToUpdate[falseYoungs]] <- cutoffForYoungAge
+  standAgeMap[pixToUpdate[trueYoungs]] <- initialTSD[pixToUpdate[trueYoungs]]
 
   return(standAgeMap)
 }
@@ -47,22 +52,29 @@ makeTSD <- function(year, firePolys, standAgeMap, lcc) {
 #' @param years the years over which to iterate
 #' @param fireBufferedListDT data.table containing non-annual burn and buffer pixelIDs
 #' @param annualCovariates list of data.table objects with pixelID
+#' @inheritParams castCohortData
 #'
 #' @return a raster layer with unified standAge and time-since-disturbance values
 #'
 #' @export
 #' @importFrom raster raster setValues
 #' @importFrom data.table data.table
-calcYoungAge <- function(years, annualCovariates, standAgeMap, fireBufferedListDT) {
+calcYoungAge <- function(years, annualCovariates, standAgeMap, fireBufferedListDT,
+                         cutoffForYoungAge = 15) {
   #this is safest way to subset given the NULL year
   for (year in years) {
-    fires <- fireBufferedListDT[[paste0("year", year)]]
+    yearChar <- paste0("year", year)
+    ann <- annualCovariates[[yearChar]] # no copy made
+    fires <- fireBufferedListDT[[yearChar]]
     if (!is.null(fires)) {
-      pix <- annualCovariates[[paste0("year", year)]]$pixelID
-      ages <- standAgeMap[pix]
-      young <- ifelse(ages < 16, 1, 0)
-      annualCovariates[[paste0("year", year)]][, youngAge := young]
-      burnedPix <- fires[buffer == 1]$pixelID
+      set(ann, NULL, "youngAge",
+          as.integer(standAgeMap[][ann$pixelID] <= cutoffForYoungAge))
+
+      #pix <- annualCovariates[[yearChar]]$pixelID
+      #ages <- standAgeMap[pix]
+      # young <- ifelse(ages <= cutoffForYoungAge, 1, 0)
+      #annualCovariates[[yearChar]][, youngAge := as.integer(standAgeMap[][pixelID] <= cutoffForYoungAge)]
+      burnedPix <- fires$pixelID[fires$buffer == 1]
       standAgeMap[burnedPix] <- 0
     }
     standAgeMap <- setValues(standAgeMap, getValues(standAgeMap) + 1)
