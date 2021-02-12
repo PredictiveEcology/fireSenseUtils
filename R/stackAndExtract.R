33e4c17432a129783b51a4f759a9522d55d6a7da
Ian Eddy <ianmseddy@gmail.com>
2021-02-12 11:34:25 -0800

    revamp ignition covariates

--- R/stackAndExtract.R
+++ R/stackAndExtract.R
@@ -12,6 +12,7 @@ utils::globalVariables(c(
 #' @param fires list of spatial points representing annual ignitions
 #' @param climate raster stack of climate layers with names matching \code{years}
 #' @param climVar the name of the variables in \code{climate}
+#' @param flamIndex index of flammable cells - necessary when pixels are not aggregated
 #'
 #' @return a data.frame with cell numbers, ignitions, and covariates for each year
 #'
@@ -20,8 +21,10 @@ utils::globalVariables(c(
 #' @importFrom data.table rbindlist data.table
 #' @importFrom raster extract brick
 #' @importFrom stats na.omit
-stackAndExtract <- function(years, climate, fuel, LCC, fires, climVar) {
+stackAndExtract <- function(years, climate, fuel, LCC, fires, climVar, flamIndex) {
+
   ignitionYears <- lapply(years, FUN = function(year) {
+
     fires <- fires[paste0("year", fires$YEAR) %in% year,]
     climate <- climate[[year]]
     names(climate) <- climVar
@@ -29,14 +32,14 @@ stackAndExtract <- function(years, climate, fuel, LCC, fires, climVar) {
     yearCovariates <- raster::brick(climate, fuel, LCC)
     fireDT <- raster::extract(x = yearCovariates, y = fires, cellnumbers = TRUE) %>%
       as.data.table(.)
-    fireDT <- fireDT[, nFires := .N, .(cells)]
-    fireDT <- unique(fireDT)
+    fireDT <- fireDT[, ignition := 1]
     noFireDT <- raster::getValues(yearCovariates) %>%
       as.data.table(.) %>%
       .[, cells := 1:ncell(yearCovariates)] %>%
       na.omit(.)
+    noFireDT <- noFireDT[cells %in% flamIndex]
     ignitionYear <- fireDT[noFireDT, on = c('cells', dtNames)]
-    ignitionYear[is.na(nFires), nFires := 0]
+    ignitionYear[is.na(ignition), ignition := 0]
     return(ignitionYear)
   })
   ignitionYears <- rbindlist(ignitionYears)
