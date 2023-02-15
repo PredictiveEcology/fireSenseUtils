d087b970f558485c63dbbd4951343fabbcff0974
Ian Eddy <ianmseddy@gmail.com>
2023-02-15 11:30:34 -0800

    rasterized fire - terra now in imports

--- R/rasterFireFunctions.R
+++ R/rasterFireFunctions.R
@@ -1,34 +1,69 @@
 utils::globalVariables(c(
-  "pixelID", "fireID", "ids", "id"
+  "pixelID", "fireID", "ids", "id", ".N", "x", "y"
 ))
 
-#' prepare fire
+#' this is a wrapper to simplify caching of lapply with bufferForFireRaster.
+#' Years are iteratively processed by \code{makeFireID}.
+#' @param years numeric fire years
 #' @param fireRaster a \code{SpatRaster} with values representing fire years
-#' @param year numeric fire year
 #' @template flammableRTM
 #' @param bufferForFireRaster buffer size used to group discrete patches of burned pixels as
 #' belonging to the same fire
 #' @param areaMultiplier A scalar that will buffer \code{areaMultiplier * fireSize}
-#'  @param verb Logical or numeric related to how much verbosity is printed. \code{FALSE} or
+#' @param verb Logical or numeric related to how much verbosity is printed. \code{FALSE} or
+#'  \code{0} is none. \code{TRUE} or \code{1} is some. \code{2} is much more.
+#' @param minSize The absolute minimum size of the buffer & non-buffer together. This will
+#'   be imposed after \code{areaMultiplier}.
+#' @param cores number of processor cores to use
+#' @return a list of data.table named by year, with cols \code{id}, \code{buffer},
+#' and \code{pixelID}
+#' @export
+#' @importFrom parallelly availableCores
+rasterFireBufferDT <- function(years, fireRaster, flammableRTM, bufferForFireRaster, areaMultiplier,
+                               minSize = 5000, verb = 1, cores = 1) {
+
+  maxCores <- parallelly::availableCores(constraints = "connections", omit = 1)
+  cores <- min(min(length(years), cores), maxCores)
+  if (cores > 1) {
+    out <- parallel::mclapply(years, FUN = makeFireIDs, fireRaster = fireRaster,
+                              flammableRTM = flammableRTM, bufferForFireRaster = bufferForFireRaster,
+                              areaMultiplier = areaMultiplier, minSize = minSize, verb = verb)
+  } else {
+    fireBufferListDT <- lapply(years, makeFireIDs, fireRaster = fireRaster, flammableRTM = flammableRTM,
+                               bufferForFireRaster = bufferForFireRaster,
+                               areaMultiplier = areaMultiplier, minSize = minSize, verb = verb)
+  }
+  names(fireBufferListDT) <- paste0("year", years)
+  return(fireBufferListDT)
+}
+
+#' identify each year's individual fires and buffer them accordingly
+#' @param year numeric fire year
+#' @param fireRaster a \code{SpatRaster} with values representing fire years
+#' @template flammableRTM
+#' @param bufferForFireRaster buffer size used to group discrete patches of burned pixels as
+#' belonging to the same fire
+#' @param areaMultiplier A scalar that will buffer \code{areaMultiplier * fireSize}
+#' @param verb Logical or numeric related to how much verbosity is printed. \code{FALSE} or
 #'   \code{0} is none. \code{TRUE} or \code{1} is some. \code{2} is much more.
 #' @param minSize The absolute minimum size of the buffer & non-buffer together. This will
 #'   be imposed after \code{areaMultiplier}.
-#' @return a data.table with year, fire ID, buffer status, and pixelID
+#' @return a data.table with fire ID, buffer status, and pixelID
 #' @export
 #' @importFrom data.table rbindlist data.table set
 #' @importFrom terra buffer patches
-rasterFireBufferDT <- function(fireRaster, year, flammableRTM, bufferForFireRaster, areaMultiplier, minSize, verb = 1) {
+makeFireIDs <- function(year, fireRaster, flammableRTM, bufferForFireRaster, areaMultiplier, minSize = 5000, verb = 1) {
 
   #identify this year's fires
   origIndex <- which(fireRaster[] == year)
   if (length(origIndex) == 0) {
-    return(NULL) #TODO: what should happen here? empty data.table/
+    return(NULL) #TODO: what should happen here? empty data.table
   } else{
     fireRaster[] <- NA
     fireRaster[origIndex] <- 1
     #classify into discrete fires via buffer
     fireRaster <- buffer(fireRaster, width = bufferForFireRaster, background = NA)
-    #doublecheck that no patches are lost... why are patch values greater than # of patches
+    #TODO: doublecheck that no patches are lost... why are patch values greater than # of patches
     fireRaster <- terra::patches(fireRaster)
     fireIDs <- fireRaster[origIndex]
 
@@ -50,6 +85,8 @@ rasterFireBufferDT <- function(fireRaster, year, flammableRTM, bufferForFireRast
 #' @param areaMultiplier A scalar that will buffer \code{areaMultiplier * fireSize}
 #' @param minSize The absolute minimum size of the buffer & non-buffer together. This will
 #'   be imposed after \code{areaMultiplier}.
+#' @param verb Logical or numeric related to how much verbosity is printed. \code{FALSE} or
+#'  \code{0} is none. \code{TRUE} or \code{1} is some. \code{2} is much more.
 #' @return a data.table with fire ID, buffer status, and pixelID
 #' @export
 #' @importFrom data.table rbindlist data.table set
@@ -85,6 +122,8 @@ bufferToAreaRast <- function(fireIDraster, areaMultiplier, minSize, flammableRTM
 
   temp <- fireIDraster
   fireIDraster <- raster(fireIDraster)
+  emptyDT <- data.table(pixelID = integer(0), buffer = integer(0), ids = integer(0))
+
   while ((length(loci) > 0) & (it <= maxIts)) {
     dups <- duplicated(loci)
     df <- data.table(loci = loci[!dups], ids = ids[!dups], id = seq_along(ids[!dups]))
@@ -150,14 +189,51 @@ bufferToAreaRast <- function(fireIDraster, areaMultiplier, minSize, flammableRTM
 }
 
 #' create a list of annual ignition points based on fire raster
-#' @param fireBufferListDT a list of \code{data.table} with columns \code{buffer} (1 = burned),
+#' @param fireBufferDT a \code{data.table} with columns \code{buffer} (1 = burned),
 #' \code{id} (unique fire ID), and \code{pixelID}
 #' @param flammableRTM @template flammableRTM
-#' @return a list of \code{SpatialPoints}
+#' @return a list of \code{sf} point objects
 #' @export
 #' @importFrom data.table rbindlist data.table set
-#' @importFrom terra xyFromCell
-#' @importFrom sf st_nearest_points st_centroid st_multipoint st_crs st_as_sf
-rasterFireSpreadPoints <- function(fireBufferListDT) {
-  #WIP
+#' @importFrom terra xyFromCell rast res
+#' @importFrom sf st_nearest_points st_centroid st_multipoint st_crs  "st_crs<-" st_as_sf
+#' @importFrom data.table as.data.table
+rasterFireSpreadPoints <- function(fireBufferDT, flammableRTM) {
+
+  if (inherits(flammableRTM, "RasterLayer")) {
+    flammableRTM <- rast(flammableRTM)
+  }
+
+  burnedPoints <- fireBufferDT[buffer == 1,]
+  burnLocs <- xyFromCell(flammableRTM, cell = burnedPoints$pixelID)
+  burnLocs <- as.data.table(burnLocs)
+  burnLocs$ids <- burnedPoints$ids
+
+  burnPoints <- st_as_sf(burnLocs, coords = c("x", "y"))
+  st_crs(burnPoints) <- st_crs(flammableRTM)
+
+  burnCentroidDT <- burnLocs[, .(x = mean(x), y = mean(y)), .(ids)]
+
+  burnCentroids <- st_as_sf(burnCentroidDT, coords = c("x", "y"))
+  st_crs(burnCentroids) <- st_crs(flammableRTM)
+
+  #there is likely a faster way?
+  ids <- unique(burnCentroidDT$id)
+  firePoints <- lapply(ids, FUN = function(x, p = burnPoints, c = burnCentroids){
+    p <- p[p$ids == x,]
+    c <- c[c$ids == x,]
+    #gives nearest y to each x - so nearest point to each centroid
+    nearest <- sf::st_nearest_feature(x = c, y = p)
+    p <- p[nearest,]
+    return(p)
+  })
+
+  #this is a silly step where we coerce to spatialPoints for spreadFit, for now
+  firePoints <- rbindlist(firePoints)
+  #calculate size in hectares
+  multiplier <- prod(res(flammableRTM))/10000
+  burnSizes <-fireBufferDT[buffer == 1, .(SIZE_HA = .N * multiplier), .(ids)]
+  firePoints <- firePoints[burnSizes, on = c("ids")]
+  firePoints <- st_as_sf(firePoints)
+  return(firePoints)
 }
