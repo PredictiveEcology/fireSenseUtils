2661c791ac6ebde068b3b87bd2498fa343d56408
Alex Chubaty <achubaty@for-cast.ca>
2024-05-09 00:01:11 -0600

    improve check for remotes pkg
    with bbc40394c01638717c5e740016ac985391b7202e

--- R/DEoptim_fns.R
+++ R/DEoptim_fns.R
@@ -186,11 +186,14 @@ runDEoptim <- function(landscape,
           }
 
           if (!"Require" %in% rownames(utils::installed.packages())) {
-            if (!require("remotes")) {
+            if (!requireNamespace("remotes", quietly = TRUE)) {
               install.packages("remotes")
             }
             remotes::install_github("PredictiveEcology/Require@development")
           } else if (packageVersion("Require") < "0.1.0.9000") {
+            if (!requireNamespace("remotes", quietly = TRUE)) {
+              install.packages("remotes")
+            }
             remotes::install_github("PredictiveEcology/Require@development")
           }
 
