f6a1415b2cbce233be41c7deb8b047421137ab2b
Eliot McIntire <eliotmcintire@gmail.com>
2025-10-29 09:03:00 -0700

    getFirePOlygons

--- R/getFirePolygons.R
+++ R/getFirePolygons.R
@@ -9,21 +9,26 @@
 #' @export
 #' @importFrom reproducible prepInputs
 #' @importFrom sf st_area
-getFirePolygons <- function(years, useInnerCache = FALSE, ...) {
-  currentURL <- "https://cwfis.cfs.nrcan.gc.ca/downloads/nfdb/fire_poly/current_version/NFDB_poly.zip"
+getFirePolygons <- function(url, years, useInnerCache = FALSE, ...) {
+  # This is stuck at 2021 max year
+  if (missing(url))
+    # url <- "https://cwfis.cfs.nrcan.gc.ca/downloads/nfdb/fire_poly/current_version/NFDB_poly.zip"
+    url <- "https://cwfis.cfs.nrcan.gc.ca/downloads/nbac/NBAC_1972to2024_20250506_shp.zip"
   firePolys <- prepInputs(
-    url = currentURL,
+    url = url,
     useCache = useInnerCache,
     ...
   ) ## this object will cache several gigabytes of cached for a small object
 
   firePolys$YEAR <- as.numeric(firePolys$YEAR) ## it has been character before...
+  if (is(firePolys, "SpatVector"))
+    firePolys$POLY_HA <- round(terra::expanse(firePolys, unit = "ha"), 2)
+  else
+    firePolys$POLY_HA <- round(sf::st_area(firePolys, unit = "ha"), 2)
 
   firePolygonsList <- lapply(years, FUN = function(x, polys = firePolys) {
     firePoly <- polys[polys$YEAR == x, ]
     if (nrow(firePoly) > 0) {
-      firePoly$POLY_HA <- round(st_area(firePoly, unit = "ha") / 1e4, digits = 2)
-      # firePoly <- firePoly[!duplicated(firePoly$FIRE_ID), ] ## TODO: what was this for?
       return(firePoly)
     } else {
       return(NULL)
